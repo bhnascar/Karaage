0f9c771c729bc9226b8db7026419c73477269655
Alex Wang <alex.wang@chromatiqa.org>
2015-05-07 16:01:56 -0700

    Add transform callback and fade out effect.

--- Editor (4.7 version)/Content/UIResources/javascript/hud.js
+++ Editor (4.7 version)/Content/UIResources/javascript/hud.js
@@ -1,5 +1,16 @@
 var contentTypes = ['boxes', 'cylinders', 'cones', 'spheres', 'wedges'];
+var transformTypes = ['translate', 'scale', 'rotate'];
+
+$('.menu-container').fadeOut(3000);
 
 engine.on('SetContentTypeForHud', function (index) {
-  $("#content-type").html(contentTypes[index]);
+  $('.menu-container').show();
+  $('#mode-content').html('Inserting' + contentTypes[index]);
+  $('.menu-container').fadeOut(3000);
+});
+
+engine.on('SetTransformationTypeForHud', function (index) {
+  $('.menu-container').show();
+  $('#mode-content').html('Transformation: ' + transformTypes[index]);
+  $('.menu-container').fadeOut(3000);
 });
