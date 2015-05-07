afc648d41b93a8a09ba28fd9c8f837c1620c4f2f
Alex Wang <alex.wang@chromatiqa.org>
2015-05-07 16:07:33 -0700

    Kill animation if function is called again.

--- Editor (4.7 version)/Content/UIResources/javascript/hud.js
+++ Editor (4.7 version)/Content/UIResources/javascript/hud.js
@@ -4,12 +4,18 @@ var transformTypes = ['translate', 'scale', 'rotate'];
 $('.menu-container').fadeOut(3000);
 
 engine.on('SetContentTypeForHud', function (index) {
+  if ($('.menu-container').is(':animated')) {
+    $('.menu-container').stop().animate({opacity:'100'});
+  }
   $('.menu-container').show();
   $('#mode-content').html('Inserting' + contentTypes[index]);
   $('.menu-container').fadeOut(3000);
 });
 
 engine.on('SetTransformationTypeForHud', function (index) {
+  if ($('.menu-container').is(':animated')) {
+    $('.menu-container').stop().animate({opacity:'100'});
+  }
   $('.menu-container').show();
   $('#mode-content').html('Transformation: ' + transformTypes[index]);
   $('.menu-container').fadeOut(3000);
