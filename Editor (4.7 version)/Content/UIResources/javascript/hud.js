2c0846834796a65d239cc05f46b113e7fa0faf43
Defade <shikly@gmail.com>
2015-05-07 03:38:38 -0700

    Basic HUD

--- Editor (4.7 version)/Content/UIResources/javascript/hud.js
+++ Editor (4.7 version)/Content/UIResources/javascript/hud.js
@@ -1,5 +1,5 @@
 var contentTypes = ['boxes', 'cylinders', 'cones', 'spheres', 'wedges'];
 
-engine.on(SetContentTypeForHud, function (index) {
+engine.on('SetContentTypeForHud', function (index) {
   $("#content-type").html(contentTypes[index]);
 });
