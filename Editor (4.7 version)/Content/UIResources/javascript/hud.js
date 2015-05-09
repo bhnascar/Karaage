bf49ecec42e08f90b82917de727c57227d52e013
Defade <shikly@gmail.com>
2015-05-09 05:42:30 -0700

    Fixing drop-to-ground. Added snapping to grid for scaling. Cleaned up BP a bit.

--- Editor (4.7 version)/Content/UIResources/javascript/hud.js
+++ Editor (4.7 version)/Content/UIResources/javascript/hud.js
@@ -1,5 +1,22 @@
 var contentTypes = ['boxes', 'cylinders', 'cones', 'spheres', 'wedges'];
-
+var transformTypes = ['translate', 'scale', 'rotate'];
+ 
+$('.menu-container').fadeOut(3000);
+ 
 engine.on('SetContentTypeForHud', function (index) {
-  $("#content-type").html(contentTypes[index]);
+  if ($('.menu-container').is(':animated')) {
+    $('.menu-container').stop().animate({opacity:'100'});
+  }
+  $('.menu-container').show();
+  $('#mode-content').html('Inserting ' + contentTypes[index]);
+  $('.menu-container').fadeOut(3000);
+});
+ 
+engine.on('SetTransformationTypeForHud', function (index) {
+  if ($('.menu-container').is(':animated')) {
+    $('.menu-container').stop().animate({opacity:'100'});
+  }
+  $('.menu-container').show();
+  $('#mode-content').html('Transformation: ' + transformTypes[index]);
+  $('.menu-container').fadeOut(3000);
 });
