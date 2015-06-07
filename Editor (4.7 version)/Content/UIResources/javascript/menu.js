163b81b6866a4b3328546534607d3845c6356e95
mochimoshi <alex.wang@chromatiqa.org>
2015-06-07 03:24:59 -0700

    Add mouseover effects and selection actions to menu.

--- Editor (4.7 version)/Content/UIResources/javascript/menu.js
+++ Editor (4.7 version)/Content/UIResources/javascript/menu.js
@@ -59,7 +59,7 @@ $( "#new-level-confirmation-cancel" ).click(function( event ) {
 });
 
 // Quit confirmation menu
-$( "#save-confirmation-cancel" ).click(function( event ) {
+$( "#quit-confirmation-cancel" ).click(function( event ) {
   event.preventDefault();
   $("#save-confirmation-container").fadeOut(250, function() {
     $(".list-group-item").removeClass('active');
@@ -68,6 +68,7 @@ $( "#save-confirmation-cancel" ).click(function( event ) {
     });
   });
 });
+
 
 engine.on('3DMouseRotateX', function (argument) {
   var menuCount = $(".list-group-item:visible").length;
@@ -85,48 +86,84 @@ engine.on('3DMouseRotateX', function (argument) {
   $(".list-group-item:visible").eq(activeIndex).addClass('active');
 });
 
+// Get row of item that is currently looked at
+var currentRow = 0;
 
-// Keyboard shortcuts for testing
-$(document).keydown(function(e) {
-    switch(e.which) {
-        case 13: { // enter
-          $(".active").click();
-          break;
-        }
+engine.on('GazeInput', function(yPosition, xPosition) {
+  // 12 row system, so multiply percentage by 12.
 
-        case 37:  {// left
-          break;
-        }
+  var newRow = Math.ceil(xPosition * 12);
+  if (newRow == 0) {
+    newRow = 1;
+  }
 
-        case 38: // up
-          var menuCount = $(".list-group-item:visible").length;
+  // $(".menu-button-item").removeClass("hovered");
 
-          var activeItem = $(".active");
-          var activeIndex = activeItem.parent().children().index(activeItem);
-
-          activeIndex = (activeIndex - 1 + menuCount) % menuCount;
-
-          $(".list-group-item").removeClass('active');
-          $(".list-group-item:visible").eq(activeIndex).addClass('active');
+  var selectedItem = null;
+  if (currentRow != newRow) {
+    currentRow = newRow;
+    switch (currentRow) {
+      case 1:
+        selectedItem = $(".highlight-row-1:visible").find(".menu-button-item");
         break;
-
-        case 39: { // right
-          break;
-        }
-
-        case 40: // down
-          var menuCount = $(".list-group-item:visible").length;
-
-          var activeItem = $(".active");
-          var activeIndex = activeItem.parent().children().index(activeItem);
-
-          activeIndex = (activeIndex + 1) % menuCount;
-
-          $(".list-group-item").removeClass('active');
-          $(".list-group-item:visible").eq(activeIndex).addClass('active');
+      case 2:
+        selectedItem = $(".highlight-row-2:visible").find(".menu-button-item");
         break;
-
-        default: return; // exit this handler for other keys
+      case 3:
+        selectedItem = $(".highlight-row-3:visible").find(".menu-button-item");
+        break;
+      case 4:
+        selectedItem = $(".highlight-row-4:visible").find(".menu-button-item");
+        break;
+      case 5:
+        selectedItem = $(".highlight-row-5:visible").find(".menu-button-item");
+        break;
+      case 6:
+        selectedItem = $(".highlight-row-6:visible").find(".menu-button-item");
+        break;
+      case 7:
+        selectedItem = $(".highlight-row-7:visible").find(".menu-button-item");
+        break;
+      case 8:
+        selectedItem = $(".highlight-row-8:visible").find(".menu-button-item");
+        break;
+      case 9:
+        selectedItem = $(".highlight-row-9:visible").find(".menu-button-item");
+        break;
+      case 10:
+        selectedItem = $(".highlight-row-10:visible").find(".menu-button-item");
+        break;
+      case 11:
+        selectedItem = $(".highlight-row-11:visible").find(".menu-button-item");
+        break;
+      case 12:
+        selectedItem = $(".highlight-row-12:visible").find(".menu-button-item");
+        break;
+      default:
+        selectedItem = $(".highlight-row-1:visible").find(".menu-button-item");
+        break;
     }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
+
+    var currentHoveredItem = $(".hovered:visible");
+
+    if (currentHoveredItem != selectedItem) {
+      engine.trigger("Print Line", "Changing item from " + currentHoveredItem + " to " + selectedItem);
+      currentHoveredItem.removeClass("hovered");
+      selectedItem.addClass("hovered");
+    }
+  }
 });
+
+// Click menu item
+engine.on('MenuClicked', function() {
+  engine.trigger("Clicking...");
+  var activeItem = $(".hovered:visible");
+
+  activeItem.click();
+});
+
+var counter = 0;
+setInterval(function() {
+  engine.trigger("Event Save Level", "Test " + counter);
+  counter++;
+}, 1000);
